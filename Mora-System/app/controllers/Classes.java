7d622415eb7c8ae4e32d322c01633c3bcee8474b
Zein <abdelrahman.zein@gmail.com>
2012-02-05 09:12:52 +0200

    created the Class.java class

---
+++ Mora-System/app/controllers/Classes.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import models.Student;
+import controllers.CRUD.ObjectType;
+import play.*;
+import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
+import play.mvc.*;
+
+@With(Secure.class)
+public class Classes extends CRUD {
+
+	public static void view() throws Exception {
+		
+	}
+
+}
