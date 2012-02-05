f95fdc29ed0d1a206d51f549df72ac1524c42fca
Zein <abdelrahman.zein@gmail.com>
2012-02-05 09:07:35 +0200

    created the Class.java class

--- Mora-System/app/models/Class.java
+++ Mora-System/app/models/Class.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 import java.util.LinkedList;
 
 import javax.persistence.Entity;
+
+import controllers.Security;
 import play.data.validation.*;
 import play.db.jpa.Model;
 
@@ -28,10 +30,44 @@ public class Class extends Model {
 	 * The teaching assistants in this class 
 	 */
 	@Required
-	LinkedList<Student> TAs = new LinkedList<Student>();
+	LinkedList<Student> teachingAssistants = new LinkedList<Student>();
+	/*
+	 * The students of this class
+	 */
+	@Required
+	LinkedList<Student> students = new LinkedList<Student>();
 	/*
 	 * The academic level at which this class currently stands
 	 */
 	@Required
 	int level;
+	/*
+	 * The date of the first session for this class
+	 */
+	@Required
+	public Date startDate;
+	
+	public Class(String sector, Student teacher, int level, Date startDate){
+		this.sector = sector;
+		this.teacher = teacher;
+		this.level = level;
+		this.startDate = startDate;
+	}
+	
+	public static Class getClassByTeacherEmail(String teacherEmail) {
+		return find("teacher", Student.find("byEmail", teacherEmail)).first();
+	}
+	
+	public Student getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Student teacher) {
+		this.teacher = teacher;
+	}
+	
+	
+	
+	
+	
 }
